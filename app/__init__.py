a49c2189e74155a18df4344b14bdf4889dae984a
Vasily Sovetov <vsovetov@croc.ru>
2017-11-14 19:57:37 +0300

    Update init
    * Add Bootstrap
    * Add views import
    * Add login manager
    * Add db initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "supersecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/database.db"
+
+Bootstrap(app)
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = "login"
+
+from app import views # noqa: ignore=E402,F401
+from app import models # noqa: ignore=E402,F401
