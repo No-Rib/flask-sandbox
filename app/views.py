71c6e6e1fbf102a78cb6652b178531f21ef3b253
Vasily Sovetov <vsovetov@croc.ru>
2018-01-01 17:48:41 +0300

    Add blog post creation view

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import app, db
-from app.forms import LoginForm, SignupForm
-from app.models import UserModel
+from app.forms import LoginForm, SignupForm, PostForm
+from app.models import UserModel, PostModel
 
 
 @app.route("/")
@@ -15,18 +15,18 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    login_form = LoginForm()
 
-    if form.validate_on_submit():
-        user = UserModel.query.filter_by(username=form.username.data).first()
-        if user is not None and check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+    if login_form.validate_on_submit():
+        user = UserModel.query.filter_by(username=login_form.username.data).first()
+        if user is not None and check_password_hash(user.password, login_form.password.data):
+            login_user(user, remember=login_form.remember.data)
             return redirect(url_for("dashboard"))
         else:
             return "<h1>Invalid username or password!</h1>"
         # return "<h1>{0} {1}</h1>".format(form.username.data, form.password.data)
 
-    return render_template("login.html", form=form)
+    return render_template("login.html", form=login_form)
 
 
 @app.route("/logout")
@@ -38,20 +38,26 @@ def logout():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    form = SignupForm()
+    signup_form = SignupForm()
 
-    if form.validate_on_submit():
+    print "SU FORM {0} {1} {2}".format(
+        signup_form.email.data,
+        signup_form.username.data,
+        signup_form.password.data
+    )
+
+    if signup_form.validate_on_submit():
         new_user = UserModel(
-            email=form.email.data,
-            username=form.username.data,
-            password=generate_password_hash(form.password.data, method="sha256")
+            email=signup_form.email.data,
+            username=signup_form.username.data,
+            password=generate_password_hash(signup_form.password.data, method="sha256"),
         )
         db.session.add(new_user)
         db.session.commit()
 
-        return "<h1>User {0} has been created!</h1>".format(form.username.data)
+        return "<h1>User {0} has been created!</h1>".format(signup_form.username.data)
 
-    return render_template("signup.html", form=form)
+    return render_template("signup.html", form=signup_form)
 
 
 @app.route("/dashboard")
@@ -61,3 +67,26 @@ def dashboard():
         "name": current_user.username
     }
     return render_template("dashboard.html", **kwargs)
+
+
+@app.route("/new_post", methods=["GET", "POST"])
+@login_required
+def create_post():
+    post_form = PostForm()
+
+    print "CURRENT USER: {0}".format(current_user)
+    print "FORM IS VALID: {0}".format(post_form.validate_on_submit())
+    print "FORM: {0} {1}".format(post_form.header.data, post_form.body.data)
+
+    if post_form.validate_on_submit():
+        new_post = PostModel(
+            header=post_form.header.data,
+            body=post_form.body.data,
+            user_id=current_user.username,
+        )
+        db.session.add(new_post)
+        db.session.commit()
+
+        return "<h1>New post has been created!</h1>"
+
+    return render_template("new_post.html", form=post_form)
