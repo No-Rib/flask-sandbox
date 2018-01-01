08c079804a18179cead6b65a4f7c85020eafef84
Vasily Sovetov <vsovetov@croc.ru>
2018-01-01 17:47:49 +0300

    Add model for blog post

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from app import db, lm
 
 
 class UserModel(UserMixin, db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(15), unique=True)
@@ -17,3 +19,16 @@ class UserModel(UserMixin, db.Model):
 @lm.user_loader
 def load_user(user_id):
     return UserModel.load(user_id)
+
+
+class PostModel(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    header = db.Column(db.String(80))
+    body = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    @classmethod
+    def load(cls, post_id):
+        return cls.query.get(post_id)
