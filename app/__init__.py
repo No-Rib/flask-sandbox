d9c7e1893fb543378b69bec35a59fe38da09b8c2
Vasily Sovetov <vsovetov@croc.ru>
2018-01-08 14:28:05 +0300

    Add redis initialization to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_redis import FlaskRedis
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,11 +10,15 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "supersecret"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/database.db"
 
+app.config["REDIS_URL"] = "redis://localhost:6379"
+
 Bootstrap(app)
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = "login"
+
+redis = FlaskRedis(app)
 
 from app import views # noqa: ignore=E402,F401
 from app import models # noqa: ignore=E402,F401
